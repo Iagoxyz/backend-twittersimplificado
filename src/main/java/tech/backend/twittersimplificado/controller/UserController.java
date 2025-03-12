6ee2c73eeefea6dc4e1338778e937d4114f7fb96
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-03-12 12:11:22 -0300

    Creating the Create New User functionality

--- src/main/java/tech/backend/twittersimplificado/controller/UserController.java
+++ src/main/java/tech/backend/twittersimplificado/controller/UserController.java
@@ -1,19 +1,52 @@
 package tech.backend.twittersimplificado.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import tech.backend.twittersimplificado.controller.dto.CreateUserDTO;
+import tech.backend.twittersimplificado.entity.Role;
+import tech.backend.twittersimplificado.entity.User;
+import tech.backend.twittersimplificado.repository.RoleRepository;
 import tech.backend.twittersimplificado.repository.UserRepository;
+
+import java.util.Set;
 
 @RestController
 public class UserController {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
-    public ResponseEntity<Void> newUser()
+    @Transactional
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDTO dto) {
+
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+        var userFromDB =  userRepository.findByUsername(dto.username());
+
+        if (userFromDB.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        var user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
 }
